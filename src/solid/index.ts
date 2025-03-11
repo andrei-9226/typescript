336f78681fdbee2aaa7806031f77ae80891e7300
andrei-9226 <andreiworkgo@gmail.com>
2025-03-11 20:27:56 +0300

    feat: add dip for class

--- src/solid/index.ts
+++ src/solid/index.ts
@@ -1,3 +1,4 @@
 import "./1_single_responsibility/users_fc";
 import "./2_open_closed/ocp";
-import "./3_liskov_substitution/lsp"
+import "./3_liskov_substitution/lsp";
+import "./5_dependency_inversion/dip";
