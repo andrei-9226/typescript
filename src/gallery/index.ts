d6263dd858b29f81d5ea13be2c26ab71b8c88f77
andrei-9226 <andreiworkgo@gmail.com>
2025-03-11 23:20:48 +0300

    feat: add control with button

---
+++ src/gallery/index.ts
@@ -0,0 +1,5 @@
+import "./core/control";
+
+const galleryApp = document.querySelector("#gallery") as HTMLElement;
+const imagesContainer = document.querySelector("[data-gallery-images]")!;
+const buttonsControl = document.querySelector("[data-control]")!;
