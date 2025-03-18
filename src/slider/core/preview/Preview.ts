f360d3a3cc8831166c1796919554612ed7cb8b02
andrei-9226 <andreiworkgo@gmail.com>
2025-03-18 15:14:32 +0300

    refactor: refactor preview class

--- src/slider/core/preview/Preview.ts
+++ src/slider/core/preview/Preview.ts
@@ -17,7 +17,7 @@ class Preview {
   ) {
     this.store = store;
     this.listPreview = [...listItems].map((item) => {
-      const img = domElement.createImageElement(item.src, "", 100, 100);
+      const img = domElement.createImageElement(item.src, "", 120, 100);
       img.className = "slider__preview-item";
       return img;
     });
@@ -25,6 +25,7 @@ class Preview {
       "slider__active-preview",
     ]);
     root.append(this.activeRunner, ...this.listPreview);
+    this.store.addChangeSlideListener(this.setRunnerTranslate);
     this.initPreview();
   }
 
@@ -32,15 +33,7 @@ class Preview {
     this.listPreview.forEach((item, index) =>
       item.addEventListener("click", () => this.onSetActiveSlide(index))
     );
-    const item = getSizeElementWithoutPadding(this.listPreview[0]);
-    const { paddingTop, paddingLeft } = getPaddingElement(this.listPreview[0]);
-    console.log(paddingTop);
-
-    this.activeRunner.style.width = `${item.width}px`;
-    this.activeRunner.style.height = `${item.height}px`;
-    this.activeRunner.style.top = `${paddingTop}px`;
-    this.activeRunner.style.left = `${paddingLeft}px`;
-    this.runnerFactor = paddingLeft * 2 + item.width;
+    this.setStyleToRunner();
   }
 
   onSetActiveSlide = (numberSlide: number) => {
@@ -51,12 +44,28 @@ class Preview {
     this.setRunnerTranslate();
   };
 
-  setRunnerTranslate() {
+  setRunnerTranslate = () => {
+    const { currentActiveSlide, numberOfSlides } = this.store.state;
+    if (numberOfSlides && currentActiveSlide > numberOfSlides) {
+      return;
+    }
     const transformValue =
       this.store.state.currentActiveSlide * this.runnerFactor -
       this.runnerFactor;
     this.activeRunner.style.transform =
       "translate(" + transformValue + "px, 0)";
+  };
+
+  setStyleToRunner() {
+    const SIDES_OF_PADDING = 2;
+    const item = getSizeElementWithoutPadding(this.listPreview[0]);
+    const { paddingTop, paddingLeft } = getPaddingElement(this.listPreview[0]);
+
+    this.activeRunner.style.width = `${item.width}px`;
+    this.activeRunner.style.height = `${item.height}px`;
+    this.activeRunner.style.top = `${paddingTop}px`;
+    this.activeRunner.style.left = `${paddingLeft}px`;
+    this.runnerFactor = paddingLeft * SIDES_OF_PADDING + item.width;
   }
 }
 
